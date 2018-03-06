a4e0b2aec1a5b05e490e445563b2a9c7cef1acd2
Dale Nguyen <dungnq@itbox4vn.com>
2018-03-06 13:15:28 -0500

    Added refresher to team detail

--- src/services/elite-api/elite-api.service.ts
+++ src/services/elite-api/elite-api.service.ts
@@ -3,12 +3,14 @@ import { Http, Response } from '@angular/http';
 
 import 'rxjs';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 @Injectable()
 export class EliteApiService{
 
   private baseUrl = 'https://elite-schedule-app-i2-dn.firebaseio.com/';
-  currentTourney : any = {};
+  private currentTourney : any = {};
+  private tourneyData = {};
 
   constructor(private http: Http){}
 
@@ -19,15 +21,28 @@ export class EliteApiService{
     })
   }
 
-  getTournamentData(tourneyId) : Observable<any>{
+  getTournamentData(tourneyId, forceRefresh: boolean = false) : Observable<any>{
+    if(!forceRefresh && this.tourneyData[tourneyId]){
+      this.currentTourney = this.tourneyData[tourneyId];
+      console.log('**no need to make HTTP call, just return the data');
+      return Observable.of(this.currentTourney);
+    }
+
+    // Don't have data yet
+    console.log('**about to make HTTP call')
     return this.http.get(`${this.baseUrl}/tournaments-data/${tourneyId}.json`)
         .map((response: Response) => {
-          this.currentTourney = response.json();
+          this.tourneyData[tourneyId] = response.json();
+          this.currentTourney = this.tourneyData[tourneyId];
           return this.currentTourney;
         })
   }
 
   getCurrentTourney(){
     return this.currentTourney;
+  }
+
+  refreshCurrentTourney(){
+    return this.getTournamentData(this.currentTourney.tournament.id, true);
   }
 }
