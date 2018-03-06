797109010dd313993d60b9e6a8339d1f52d63d6f
Dale Nguyen <dungnq@itbox4vn.com>
2018-03-06 16:17:56 -0500

    Added android emulator

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavParams } from 'ionic-angular';
 import { EliteApiService } from './../../services/services';
 
 declare var window: any;
